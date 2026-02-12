989ea974569487578bae28ecc8242ad81dd890d8
Joalison Ferreira <joalison.ferreira@nefex.io>
2026-02-11 23:30:07 -0300

    feat: idea creation with corresponding services and resolvers

---
+++ src/resolvers/idea.resolver.ts
@@ -0,0 +1,16 @@
+import { Arg, Mutation, Resolver } from "type-graphql";
+import { IdeaService } from "../services/idea.service";
+import { IdeaModel } from "../models/idea.model";
+import { CreateIdeaInput } from "../dtos/input/idea.input";
+
+@Resolver(() => IdeaModel)
+export class IdeaResolver {
+  private ideaService = new IdeaService();
+
+  @Mutation(() => IdeaModel)
+  async createIdea(
+    @Arg("data", () => CreateIdeaInput) data: CreateIdeaInput,
+  ): Promise<IdeaModel> {
+    return this.ideaService.createIdea(data);
+  }
+}
--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -1,7 +1,8 @@
-import { Arg, Query, Resolver, UseMiddleware } from "type-graphql";
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
 import { UserModel } from "../models/user.model";
 import { UserService } from "../services/user.service";
 import { IsAuth } from "../middlewares/auth.middleware";
+import { CreateUserInput } from "../dtos/input/user.input";
 
 @Resolver(() => UserModel)
 @UseMiddleware(IsAuth)
@@ -11,5 +12,12 @@ export class UserResolver {
   @Query(() => UserModel)
   async getUser(@Arg("id", () => String) id: string): Promise<UserModel> {
     return this.userService.findUser(id);
+  }
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Arg("data", () => CreateUserInput) data: CreateUserInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(data);
   }
 }
